5d4fb431d42ab7a18d528ee7152680645d6ed108
RacFor4k <racfor4@gmail.com>
2025-04-12 11:37:01 +0300

    Написал инициализатор minifilter

--- ProcessMonitor/Main.cpp
+++ ProcessMonitor/Main.cpp
@@ -4,14 +4,8 @@
 PFLT_FILTER gFilterHandle = NULL;
 
 FLT_OPERATION_REGISTRATION Callbacks[] = {
-    {
-        IRP_MJ_CREATE,
-        0,
-        nullptr,          // Pre-operation callback
-        nullptr           // Post-operation callback
-    },
-    { IRP_MJ_READ, 0, MiniFilter::PreReadCallback, nullptr},
-    {IRP_MJ_WRITE, 0, MiniFilter::PreWriteCallback, nullptr}
+    { IRP_MJ_READ, 0, MiniFilter::PreReadCallback, nullptr },
+    {   IRP_MJ_WRITE, 0, MiniFilter::PreWriteCallback, nullptr },
     { IRP_MJ_OPERATION_END }
 };
 
@@ -32,8 +26,27 @@ const FLT_REGISTRATION FilterRegistration = {
 };
 
 NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
-	NTSTATUS status;
-	PFLT_PORT serverPort = NULL;
+    UNREFERENCED_PARAMETER(RegistryPath);
 
-	status = FltRegisterFilter(DriverObject, &FilterRegistration, &gFilterHandle)
+    NTSTATUS status = FltRegisterFilter(DriverObject, &FilterRegistration, &gFilterHandle);
+    if (!NT_SUCCESS(status)) {
+        return status;
+    }
+
+    status = FltStartFiltering(gFilterHandle);
+    if (!NT_SUCCESS(status)) {
+        FltUnregisterFilter(gFilterHandle);
+    }
+
+    return status;
+}
+
+NTSTATUS MiniFilterUnload(
+    _In_ FLT_FILTER_UNLOAD_FLAGS Flags
+)
+{
+    UNREFERENCED_PARAMETER(Flags);
+
+    FltUnregisterFilter(gFilterHandle);
+    return STATUS_SUCCESS;
 }
