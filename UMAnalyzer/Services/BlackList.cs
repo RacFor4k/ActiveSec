3909c5f0e90e0597e3ea3061092c099fafa91071
RacFor4k <racfor4@gmail.com>
2025-08-17 04:06:40 +0300

    Добавлены AddProcess, AddThreat, LoadProcesses, UpdateProcessesList, UpdateThreatsList в BalckList

--- UMAnalyzer/Services/BlackList.cs
+++ UMAnalyzer/Services/BlackList.cs
@@ -11,10 +11,12 @@ namespace UMAnalyzer.Services
 {
     internal class BlackList
     {
-        private HashSet<Threat> _ThreatList;
+        private HashSet<Threat> _ThreatList = new HashSet<Threat>();
         private HashSet<Process> _ProcessesList = new HashSet<Process>();
         private void LoadThreats(string path)
         {
+            _ThreatList = new HashSet<Threat>();
+
             foreach (var line in File.ReadLines(path))
             {
                 if (string.IsNullOrWhiteSpace(line))
@@ -22,28 +24,74 @@ namespace UMAnalyzer.Services
 
                 try
                 {
-                    using var doc = JsonDocument.Parse(line);
-                    if (doc.RootElement.TryGetProperty("ProcessPath", out var value))
-                    {
-                        _ThreatList.Add(new Threat { ProcessPath = value.GetString() });
-                    }
+                    _ThreatList.Add(JsonSerializer.Deserialize<Threat>(line));
                 }
                 catch (JsonException ex)
                 {
-                    Console.WriteLine($"Ошибка парсинга строки: {line} \n{ex.Message}");
+                    Console.WriteLine($"Ошибка парсинга строки: {line}\n{ex.Message}");
                 }
             }
-        } 
+        }
+
+        private void UpdateThreatsList(string path, Threat threat)
+        {
+            using (var fs = File.AppendText(path))
+            {
+                string json = JsonSerializer.Serialize(threat);
+                fs.WriteLine(json); // автоматически добавляет перевод строки
+            }
+        }
+
+        private void UpdateProcessesList(string path, Process threat)
+        {
+            using (var fs = File.AppendText(path))
+            {
+                string json = JsonSerializer.Serialize(threat);
+                fs.WriteLine(json); // автоматически добавляет перевод строки
+            }
+        }
+
+        private void LoadProcesses(string path)
+        {
+            _ProcessesList = new HashSet<Process>();
+
+            foreach (var line in File.ReadLines(path))
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                try
+                {
+                    _ProcessesList.Add(JsonSerializer.Deserialize<Process>(line));
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"Ошибка парсинга строки: {line}\n{ex.Message}");
+                }
+            }
+        }
 
         public BlackList()
         {
-            _ThreatList = new HashSet<Threat>();
             LoadThreats(ConstProvider.ThreatsPath);
+            LoadProcesses(ConstProvider.ProcessesPath);
         }
 
         public Process? GetProcess(string ProcessPath)
         {
             return _ProcessesList.First(t => t.Equals(new Process(ProcessPath)));
+        }
+        public void AddProcess(Process process)
+        {
+            if(_ProcessesList.Add(process))
+                UpdateProcessesList(ConstProvider.ProcessesPath, process);
+        }
+        public void AddThreat(Process threatProcess)
+        {
+            Threat threat = new Threat { ProcessPath = threatProcess.GetProcessPath() };
+            if(_ThreatList.Add(threat))
+                UpdateThreatsList(ConstProvider.ThreatsPath, threat);
+            _ProcessesList.Remove(threatProcess);
         }
     }
 }
