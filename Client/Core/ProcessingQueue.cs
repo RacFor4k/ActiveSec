0a70da511242c7424a34f387898a53ae3fa1edee
RacFor4k <racfor4@gmail.com>
2025-11-22 23:51:05 +0300

    Reapply "изменена система аунтефикации"
    This reverts commit 5d95e1698f455cb5f2dacef23497b5549d3ba7be.

---
+++ Client/Core/ProcessingQueue.cs
@@ -0,0 +1,42 @@
+﻿using Client.Native;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Core
+{
+    public class QueueItem
+    {
+        public DRIVER_MSG_HEADER Header { get; set; }
+    }
+
+    public class ProcessingQueue
+    {
+        private readonly BlockingCollection<QueueItem> _queue;
+
+        public ProcessingQueue()
+        {
+            _queue = new BlockingCollection<QueueItem>(new ConcurrentQueue<QueueItem>());
+        }
+
+        public void Add(QueueItem item)
+        {
+            _queue.Add(item);
+        }
+
+        public QueueItem? Take(CancellationToken cancellationToken)
+        {
+            try
+            {
+                return _queue.Take(cancellationToken);
+            }
+            catch (OperationCanceledException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Client/Native/NativeMethods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Native
+{
+    internal static class NativeMethods
+    {
+        // https://learn.microsoft.com/en-us/windows-hardware/drivers/ifs/filterconnectcommunicationport
+        [DllImport("FltLib.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern int FilterConnectCommunicationPort(
+            string lpPortName,
+            uint dwOptions,
+            IntPtr lpContext,
+            uint dwSizeOfContext,
+            IntPtr lpSecurityAttributes,
+            out IntPtr hPort);
+
+        // https://learn.microsoft.com/en-us/windows-hardware/drivers/ifs/filtersendmessage
+        [DllImport("FltLib.dll", SetLastError = true)]
+        public static extern int FilterSendMessage(
+            IntPtr hPort,
+            IntPtr lpInBuffer,
+            uint dwInBufferSize,
+            IntPtr lpOutBuffer,
+            uint dwOutBufferSize,
+            out uint lpBytesReturned);
+
+        // https://learn.microsoft.com/en-us/windows-hardware/drivers/ifs/filtergetmessage
+        [DllImport("FltLib.dll", SetLastError = true)]
+        public static extern int FilterGetMessage(
+            IntPtr hPort,
+            IntPtr lpMessageBuffer,
+            uint dwMessageBufferSize,
+            out uint lpBytesReturned,
+            IntPtr lpOverlapped);
+
+        // https://learn.microsoft.com/en-us/windows-hardware/drivers/ifs/filterreplymessage
+        [DllImport("FltLib.dll", SetLastError = true)]
+        public static extern int FilterReplyMessage(
+            IntPtr hPort,
+            IntPtr lpReplyBuffer,
+            uint dwReplyBufferSize);
+
+        // https://learn.microsoft.com/en-us/windows/win32/api/handleapi/nf-handleapi-closehandle
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(IntPtr hObject);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct FILTER_MESSAGE_HEADER{
+            public uint ReplyLength;
+            public ulong MessageId;
+        }
+}
+}
